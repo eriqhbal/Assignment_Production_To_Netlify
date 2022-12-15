08cdcc38e027ea54eb765332634719900ee41292
Zuleriqhbal <eriqhbal@gmail.com>
2022-12-15 22:42:06 +0700

    siap untuk di deploy

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Zuleriqhbal Hendri" // TODO: replace this
+const studentId = "FE2690570" // TODO: replace this
+
+export { url, studentName, studentId }
